237c3e76656b439b4a15be17887d04f393cfba56
Mala Nimalasuriya <malanimalasuriya@Malas-MacBook-Air.local>
2015-11-08 14:17:06 -0500

    Add css and organized links.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :comments
-  # validates :image, presence: true
-  #
-  # has_attached_file :image
-  # validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  belongs_to :user
+  validates :image, presence: true
+
+  has_attached_file :image, styles: { :medium => "640x"}
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+  has_many :posts
   # Did not include has-secure password bc used sorcery gem
 
   validates_confirmation_of :password, message: "should match confirmation", if: :password
